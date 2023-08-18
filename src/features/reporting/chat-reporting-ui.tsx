6c7c3634e9679e317ac6ac3c2de291fe8a8ac798
Inovix <info@inovix.ai>
2023-08-17 20:05:45 -0700

    Revert "updated chat history"
    This reverts commit 030d4b749e07c666b21d1b287f1842a58d544f1d.

--- src/features/reporting/chat-reporting-ui.tsx
+++ src/features/reporting/chat-reporting-ui.tsx
@@ -8,36 +8,10 @@ interface Props {
   chatId: string;
 }
 
-import { useAuth } from "@/features/auth/auth-api";
-import ChatRow from "@/components/chat/chat-row";
-import { Card } from "@/components/ui/card";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { FC, useEffect, useState } from "react";
-import { FindAllChatsInThread, FindChatThreadByID } from "./reporting-service";
-
-interface Props {
-  chatId: string;
-}
-
-export const ChatReportingUI: FC<Props> = (props) => {
-  const { user } = useAuth();
-  const [chats, setChats] = useState([]);
-  const [chatThread, setChatThread] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      const chatThreads = await FindChatThreadByID(props.chatId);
-      const allChats = await FindAllChatsInThread(props.chatId);
-      const userChats = allChats.filter((chat) => chat.userId === user?.id);
-      setChats(userChats);
-      setChatThread(chatThreads[0]);
-    }
-    fetchData();
-  }, [props.chatId, user]);
-
-  if (!chatThread) {
-    return null;
-  }
+export const ChatReportingUI: FC<Props> = async (props) => {
+  const chatThreads = await FindChatThreadByID(props.chatId);
+  const chats = await FindAllChatsInThread(props.chatId);
+  const chatThread = chatThreads[0];
 
   return (
     <Card className="h-full relative">

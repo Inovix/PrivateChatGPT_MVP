512af8631d2668cb0c440d2f9b2855249ea3f82e
Inovix <info@inovix.ai>
2023-08-17 20:02:13 -0700

    updated layout

--- src/app/reporting/layout.tsx
+++ src/app/reporting/layout.tsx
@@ -2,8 +2,8 @@ import { ProtectedPage } from "@/features/auth/protected-page";
 import { MainMenu } from "@/features/menu/menu";
 
 export const metadata = {
-  title: "AzureChatGPT",
-  description: "AzureChatGPT",
+  title: "inovix.ai PrivateChatGPT",
+  description: "inovix.ai PrivateChatGPT",
 };
 
 export default async function RootLayout({
